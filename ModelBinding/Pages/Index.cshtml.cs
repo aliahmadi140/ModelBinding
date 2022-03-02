b9530a80b8cc98d5ebf8260dda7dbecc2071c8e1
ali ahmadi <ahmadiali709@hotmail.com>
2022-03-02 00:11:53 -0800

    Add project files.

---
+++ ModelBinding/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ModelBinding.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        [BindProperty]
+        public string Animal { get; set; }
+
+
+       public IActionResult OnPost()
+        {
+            TempData["Animal"]= Animal;
+            return RedirectToPage("./Result");
+        }
+    }
+}
---
+++ ModelBinding/Pages/Result.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ModelBinding.Pages
+{
+    public class ResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
